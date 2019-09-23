237f4a5fbb0c50dd0949c596559ceeb8d8a12bd3
hqlam <hqlam@hotmail.com>
2019-09-23 11:09:46 -0700

    Delete hterm.sh

--- hterm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-osascript <<END 
-tell application "Terminal"
-    do script "cd \"`pwd`\";$2 $3 $4 $5 $6 $7 "
-    set current settings of selected tab of window 1 to settings set "$1"
-end tell
-END
